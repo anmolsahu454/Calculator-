6b37a14bae4100dcc4f8abd9d746f9e7c93be874
anmolsahu454 <anmolsahu2104@gmail.com>
2025-10-02 14:38:57 +0530

    A short, descriptive message about the changes you made

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -1,16 +1,29 @@
 import Display from "./components/Display";
 import ButtonsContainer from "./components/ButtonsContainer";
 import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
-  
+       let [calVal, setCalVal] = useState("");
+       const onButtonClick = (buttonText) => {
+           if(buttonText === "C") {
+            setCalVal("");
+           }
+           else if(buttonText === "=") {
+            const result = eval(calVal);
+            setCalVal(result.toString());
+           }
+            else {
+              const newCalVal = calVal + buttonText;
+              setCalVal(newCalVal);
+            }
+           
+       };
 
   return (
-    <div className ={styles.calculator}>
-      <Display />
-
-      <ButtonsContainer />
-
+    <div className={styles.calculator}>
+      <Display displayValue={calVal} />
+      <ButtonsContainer onButtonClick={onButtonClick} />
     </div>
   )
 }
